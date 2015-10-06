808d9d59852aa0b273e25ed1db7d035703ee7b5a
Matus Rakoci <matusrakoci@gmail.com>
2015-10-06 15:42:10 +0200

    Longest Lines solved

---
+++ 1-easy-challenges/LongestLines/src/main/java/LongestLines.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+class Main { 
+
+	private static final Comparator<String> comparator = new Comparator<String>() { 
+	
+		public int compare(String f, String t) { 
+			return Integer.compare(f.length(), t.length());
+		}
+
+	};
+
+	public static void main(String... args) throws Throwable { 
+
+		Scanner input = new Scanner(new File(args[0]));
+
+		int count = input.nextInt();
+		input.nextLine();
+
+		List<String> list = new ArrayList<String>();
+		while(input.hasNext()) 
+			list.add(input.nextLine());
+
+		Collections.sort(list, Collections.reverseOrder(comparator));
+
+		for(int i = 0; i < count; i++)
+			System.out.println(list.get(i));
+
+                input.close();
+	}
+
+}
